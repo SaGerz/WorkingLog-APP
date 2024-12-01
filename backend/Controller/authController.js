34c09044f8d89cefafc1cff9de01fa8961c3a5f1
SaGerz <genarosamuel24@gmail.com>
2024-12-01 16:02:57 +0700

    feat: add login controller

--- backend/Controller/authController.js
+++ backend/Controller/authController.js
@@ -2,13 +2,13 @@ const bycrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const db = require('../Models/db');
 
-const register = async (req, res) => {
+const registerUser = async (req, res) => {
   const {email, password, name, role} = req.body;
-
+  
   if(!email || !password || !name || !role) {
     res.status(400).json({message: "All fields are Required"});
   }
-
+  
   try {
     const checkEmailQuery = 'SELECT * FROM users WHERE email = ?';
     const [existingUser] = await db.query(checkEmailQuery, [email]);
@@ -46,4 +46,59 @@ const register = async (req, res) => {
 }
 
 
-module.exports = {register}
+const loginUser = async (req, res) => {
+  const {email, password} = req.body;
+
+  if(!email || !password){
+    res.status(400).json({message: "Email and Password are required"});
+  }
+
+  const queryUser = `SELECT * FROM users WHERE email = ?`;
+  const queryEmployee = `SELECT * FROM employees WHERE user_id = ?`
+
+  try {
+    const [userResult] = await db.query(queryUser, [email]);
+    const user = userResult[0];
+
+    if(!user) {
+      return res.status(404).json({message: "User tidak ditemukan"});
+    }
+
+    const isPassword = await bycrypt.compare(password, user.password);
+    if(!isPassword) {
+      return res.status(401).json({message: "Password Salah"})
+    }
+
+
+    const [employeeResult] = await db.query(queryEmployee, [user.id]);
+    const employee = employeeResult[0];
+
+    if (!employee) {
+      return res.status(404).json({message: "User tidak ditemukan"});
+    }
+
+    // Jwt 
+    const token = jwt.sign(
+      {userId: user.id, email: user.email, employeeId: employee.Id},
+      process.env.JWT_SECRET, {expiresIn: '1h'}
+    )
+
+    res.status(200).json({
+      message: "Login berhasil",
+      token,
+      user: {
+        id: user.id,
+        email: user.email,
+        employeeId: employee.id
+      }
+    })
+  } catch (error) {
+    console.error("Error saat login : ", error);
+    res.status(500).json({message: "Login gagal", "Error": error});
+  }
+
+}
+
+
+
+module.exports = {registerUser, loginUser}
